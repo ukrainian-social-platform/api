305ce1c9b68b982fbbd489e08abe5b574ed20c3d
KaMeHb-UA <blade.whiteblack@gmail.com>
2022-08-22 00:11:34 +0300

    use DynamicGrpcServer instead of standard ServerGrpc

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,18 @@ import { INestMicroservice } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from '@/app';
 import { MicroserviceUtilsService, CommonUtilsService } from '@/utils';
+import { DynamicGrpcServer } from '@/server';
 
 async function bootstrap() {
 	const app: INestMicroservice =
-		await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-			transport: Transport.GRPC,
-			options: {
-				get package() {
-					return app
-						.get(MicroserviceUtilsService)
-						.getGrpcPackageName();
-				},
-				get protoPath() {
-					return app.get(MicroserviceUtilsService).getGrpcProtoPath();
-				},
-			},
-		});
+		await NestFactory.createMicroservice<MicroserviceOptions>(
+			AppModule,
+			new DynamicGrpcServer(
+				() => app.get(MicroserviceUtilsService).getGrpcPackageName(),
+				() => app.get(MicroserviceUtilsService).getGrpcProtoPath(),
+				() => app.get(MicroserviceUtilsService).getGrpcServerUrl(),
+			),
+		);
 	const utils = app.get(CommonUtilsService);
 	utils.setReloadFunc(async () => {
 		await app.close();
